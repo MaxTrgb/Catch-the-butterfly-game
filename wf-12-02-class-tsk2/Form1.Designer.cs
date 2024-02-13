c15d60c7b054a3fab2280dca16b5eeddc1cc74a0
MaxTrgb <126002400+MaxTrgb@users.noreply.github.com>
2024-02-13 15:26:20 +0200

    Add project files.

---
+++ wf-12-02-class-tsk2/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿
+namespace wf_12_02_class_tsk2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            timer1 = new System.Windows.Forms.Timer(components);
+            label1 = new System.Windows.Forms.Label();
+            label2 = new System.Windows.Forms.Label();
+            timer2 = new System.Windows.Forms.Timer(components);
+            timer3 = new System.Windows.Forms.Timer(components);
+            timer4 = new System.Windows.Forms.Timer(components);
+            timer5 = new System.Windows.Forms.Timer(components);
+            pictureBox1 = new System.Windows.Forms.PictureBox();
+            pictureBox2 = new System.Windows.Forms.PictureBox();
+            pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            SuspendLayout();
+            // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = System.Drawing.Color.Transparent;
+            label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            label1.Font = new System.Drawing.Font("Impact", 36F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point);
+            label1.ForeColor = System.Drawing.Color.Tomato;
+            label1.Location = new System.Drawing.Point(596, 19);
+            label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(153, 62);
+            label1.TabIndex = 2;
+            label1.Text = "Score:";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = System.Drawing.Color.Transparent;
+            label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            label2.Font = new System.Drawing.Font("Impact", 36F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point);
+            label2.ForeColor = System.Drawing.Color.Tomato;
+            label2.Location = new System.Drawing.Point(736, 19);
+            label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(53, 62);
+            label2.TabIndex = 3;
+            label2.Text = "0";
+            // 
+            // timer2
+            // 
+            timer2.Tick += timer2_Tick;
+            // 
+            // timer3
+            // 
+            timer3.Tick += timer3_Tick;
+            // 
+            // timer4
+            // 
+            timer4.Tick += timer4_Tick;
+            // 
+            // timer5
+            // 
+            timer5.Tick += timer5_Tick;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            pictureBox1.Image = (System.Drawing.Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new System.Drawing.Point(12, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new System.Drawing.Size(38, 40);
+            pictureBox1.TabIndex = 4;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            pictureBox2.Image = (System.Drawing.Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new System.Drawing.Point(56, 12);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new System.Drawing.Size(37, 40);
+            pictureBox2.TabIndex = 5;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            pictureBox3.Image = (System.Drawing.Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new System.Drawing.Point(99, 12);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new System.Drawing.Size(40, 40);
+            pictureBox3.TabIndex = 6;
+            pictureBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            BackgroundImage = (System.Drawing.Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new System.Drawing.Size(800, 450);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Font = new System.Drawing.Font("Ink Free", 9F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point);
+            ForeColor = System.Drawing.SystemColors.ControlText;
+            Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            Name = "Form1";
+            Text = "CatchTheButterfly";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Timer timer5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ wf-12-02-class-tsk2/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.Automation;
+
+namespace wf_12_02_class_tsk2
+{
+    public partial class Form1 : Form
+    {
+        private List<PictureBox> pictures = new List<PictureBox>();
+
+        private int score = 0;
+
+        private int counter = 1;
+
+        private int livesCounter = 3;
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = 1;
+            timer1.Start();
+
+            timer2.Interval = 1000;
+            timer2.Start();
+
+            timer3.Interval = 10;
+
+            timer4.Interval = 1;
+            timer4.Start();
+
+            timer5.Interval = 1;
+            timer5.Start();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            PictureBox picture = (PictureBox)sender;
+            this.Controls.Remove(picture);
+            pictures.Remove(picture);
+            score += 1;
+            label2.Text = score.ToString();
+
+            counter += 1;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (livesCounter == 2)
+            {
+                pictureBox3.Visible = false;
+            }
+            else if (livesCounter == 1)
+            {
+                pictureBox2.Visible = false;
+            }
+            
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+
+            PictureBox picture = new PictureBox();
+            picture.Location = new Point(rand.Next(0, 700), 400);
+            picture.Size = new Size(77, 77);
+
+            // adjust the path to the image
+            picture.Image = Image.FromFile("D:\\Study\\WinForms\\wf-12-02-class-tsk2\\wf-12-02-class-tsk2\\obj\\241eee72a97d7e312b7eb28d1b7ac070.png");
+
+            picture.BackColor = Color.Transparent;
+            picture.Click += pictureBox1_Click;
+
+            if (counter >= 5)
+            {
+                timer2.Interval = 500;
+            }
+            else if (counter >= 10)
+            {
+                timer2.Interval = 300;
+            }
+            pictures.Add(picture);
+
+            this.Controls.Add(picture);
+
+            timer3.Start();
+
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            int baseSpeed = 1;
+            double speedMultiplier = 0.5;
+
+            int speed = (int)(baseSpeed + counter * speedMultiplier);
+
+            foreach (var pic in pictures)
+            {
+                Point point = Point.Add(pic.Location, new Size(0, -speed));
+                pic.Location = point;
+            }
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+
+            if (score == 25)
+            {
+                timer1.Stop();
+                timer2.Stop();
+                timer3.Stop();
+                timer4.Stop();
+                timer5.Stop();
+                MessageBox.Show("You win!");
+                return;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void timer5_Tick(object sender, EventArgs e)
+        {
+            for (int i = pictures.Count - 1; i >= 0; i--)
+            {
+                var pic = pictures[i];
+                if (pic.Location.Y <= 0)
+                {
+                    this.Controls.Remove(pic);
+                    pictures.RemoveAt(i);
+                    livesCounter -= 1;                  
+                }
+            }          
+
+            if (livesCounter == 0)
+            {
+                timer1.Stop();
+                timer2.Stop();
+                timer3.Stop();
+                timer4.Stop();
+                timer5.Stop();
+                pictureBox1.Visible = false;
+                MessageBox.Show("You lose!");
+                return;
+            }
+        }
+        
+    }
+}
